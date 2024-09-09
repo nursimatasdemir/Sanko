61c0d23261bb643a21287191ca72221161bb2fd6
nursimatasdemir <nrsmtsdmr@gmail.com>
2024-09-09 10:35:16 +0300

    Added a login button and created it's event handler. Added logic to Login_Clicked evet handler.

--- Sanko/Views/LoginPageView.xaml.cs
+++ Sanko/Views/LoginPageView.xaml.cs
@@ -1,9 +1,34 @@
+using Database;
 namespace Sanko.Views;
 
 public partial class LoginPageView : ContentPage
 {
-	public LoginPageView()
+	private readonly DataService _databaseService;
+	public LoginPageView(DataService databaseService)
 	{
 		InitializeComponent();
+		_databaseService = databaseService;
 	}
+
+    public async void Login_Clicked(object sender, EventArgs e)
+    {
+		string email = emailEntry.Text;
+		string password = passwordEntry.Text;
+
+		if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+		{
+			await DisplayAlert("Error", "Lütfen Email adresinizi ve şifrenizi giriniz!", "OK");
+			return;
+		}
+		bool isAuthenticated = await _databaseService.AuthenticateUserAsync(email, password);
+		if (isAuthenticated)
+		{
+			await Navigation.PushAsync(new MainPage());
+		} else
+		{
+			await DisplayAlert("Error", "Email adresi veya şifre yanlış!", "Tekrar Dene");
+		}
+		
+
+    }
 }
--- Sanko/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/Sanko.AssemblyInfo.cs
+++ Sanko/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/Sanko.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sanko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8cfaa570e7a5bb29adeb9771ffd22741654eafaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sanko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sanko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
